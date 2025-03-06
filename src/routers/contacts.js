709b2b68ecd0fa1bc97a673d34ba327f73e07411
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-03-06 19:32:54 +0000

    HW #3 Node

---
+++ src/routers/contacts.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    getAllContactsController,
+    getContactByIdController,
+    createContactContoller,
+    deleteContactController,
+    patchContactConroller
+} from "../controllers/contacts.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactContoller));
+router.delete('/contact/:contactId', ctrlWrapper(deleteContactController));
+router.patch('contacts/:contactId', ctrlWrapper(patchContactConroller));
+
+export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,35 @@ export const getContactById = async (contactId) => {
     const contact = await ContactsCollection.findById(contactId);
     return contact;
 };
+
+export const createContact = async (payload) => {
+    const contact = await ContactsCollection.create(payload);
+    return contact;
+};
+
+export const deleteContact = async (contactId) => {
+    const contact = await ContactsCollection.findOneAndDelete({
+        _id: contactId,
+    });
+
+    return contact;
+};
+
+export const patchContact = async (contactId, payload, options = {}) => {
+    const updatedContact = await ContactsCollection.findByIdAndUpdate(
+        { _id: contactId },
+        payload,
+        {
+            new: true,
+            includeResultMetadata: true,
+            ...options,
+        },
+    );
+
+    if (!updatedContact || !updatedContact.value) return null;
+
+    return {
+        contact: updatedContact.value,
+        isNew: Boolean(updatedContact?.lastErrorObject?.upserted),
+    };
+};
